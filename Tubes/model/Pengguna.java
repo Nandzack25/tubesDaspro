599565c97dfc5280d17f28009d6d4b562decea17
Nandzack25 <144807966+Nandzack25@users.noreply.github.com>
2023-12-18 22:56:31 +0700

    Update Pengguna.java

--- Tubes/model/Pengguna.java
+++ Tubes/model/Pengguna.java
@@ -1,12 +1,42 @@
 package model;
-public class Pengguna extends Akun{
-    private String idPengguna;
 
-    public Pengguna setIdPengguna(String idPengguna){
+public class Pengguna extends Akun {
+    private String idPengguna;
+    private int pulsaPengguna;
+    private boolean isPaketAktif;
+    private String namaPaketAktif;
+
+    public Pengguna setIdPengguna(String idPengguna) {
         this.idPengguna = idPengguna;
         return this;
     }
-    public String getIdPengguna(){
+
+    public String getIdPengguna() {
         return this.idPengguna;
+    }
+    public Pengguna setPulsaPengguna(int pulsaPengguna) {
+        this.pulsaPengguna += pulsaPengguna;
+        return this;
+    }
+
+    public int getPulsaPengguna() {
+        return this.pulsaPengguna;
+    }
+
+    public void setIsPaketAktif(boolean isPaketAktif) {
+        this.isPaketAktif = isPaketAktif;
+    }
+
+    public boolean getIsPaketAktif() {
+        return this.isPaketAktif;
+    }
+
+    public void setNamaPaketAktif(String namaPaketAktif) {
+        this.namaPaketAktif = namaPaketAktif;
+        
+    }
+
+    public String getNamaPaketAktif() {
+        return this.namaPaketAktif;
     }
 }
